fd7de646150ef858230dec277a5bf58976c8e2b8
Juan José López Delgado <juanjyce@gmail.com>
2013-01-13 23:57:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lolcat::Application.config.secret_token = 'c9b25feb134f84cb1f1e04a454e99f7a01901aee77fa0fb64b251ad12ac4c1b4325006dce47ed402b58fbdb035a43d5b9f8ba7e011a81b3472c0a7cf59b0b137'
